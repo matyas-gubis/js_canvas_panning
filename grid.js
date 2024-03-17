3190898ca630f7933298ea243efd1f13b0c8305a
matyasgubis <gubismatyas@gmail.com>
2024-03-17 22:00:28 +0100

    add zooming base

--- grid.js
+++ grid.js
@@ -19,4 +19,26 @@ export class Grid {
     }
     ctx.stroke();
   }
+
+  /**
+   * Returns the row and column number of a given cell, based on world coordinates. Indexing start from 0.
+   * @param {number} x world space coordinate x
+   * @param {number} y world space coordinate y
+   */
+  getCellByWorldCoordinates(x, y) {
+    const col = Math.floor(x / this.cellWidth);
+    const row = Math.floor(y / this.cellHeight);
+    if (col >= this.columnCount || row >= this.rowCount || col < 0 || row < 0) {
+      return null;
+    } else {
+      return { col: col, row: row };
+    }
+  }
+
+  getGridWidth() {
+    return this.columnCount * this.cellWidth;
+  }
+  getGridHeight() {
+    return this.rowCount * this.cellHeight;
+  }
 }
--- script.js
+++ script.js
@@ -7,19 +7,60 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 const grid = new Grid(10, 5, 50, 50);
+const offset = {
+  x: window.innerWidth / 2 - grid.getGridWidth() / 2,
+  y: window.innerHeight / 2 - grid.getGridHeight() / 2,
+};
+let isPanning = false;
+let mouse = {
+  x: 0,
+  y: 0,
+};
+let panning = {
+  startX: 0,
+  startY: 0,
+};
+let scale = {
+  x: 1,
+  y: 1,
+};
 
 function animate() {
+  if (isPanning) {
+    offset.x += mouse.x - panning.startX;
+    offset.y += mouse.y - panning.startY;
+
+    panning.startX = mouse.x;
+    panning.startY = mouse.y;
+  }
   draw(ctx);
   requestAnimationFrame(animate);
 }
 
+/**@param {CanvasRenderingContext2D} ctx */
 function draw(ctx) {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.translate(offset.x, offset.y);
+  ctx.scale(scale.x, scale.y);
   grid.render(ctx);
+}
+
+function screenToWorld(x, y) {
+  return {
+    x: (x - offset.x) * scale.x,
+    y: (y - offset.y) * scale.y,
+  };
+}
+
+function wordToScreen(x, y) {
+  return {
+    x: x / scale.x + offset.x,
+    y: y / scale.y + offset.y,
+  };
 }
 
 window.addEventListener("load", () => {
@@ -30,5 +71,42 @@ window.addEventListener("resize", () => {
   console.log("resize");
   draw(ctx);
 });
+window.addEventListener("click", (e) => {
+  console.log(screenToWorld(e.clientX, e.clientY));
+  const wordCoordinates = screenToWorld(e.clientX, e.clientY);
+  console.log(
+    grid.getCellByWorldCoordinates(wordCoordinates.x, wordCoordinates.y)
+  );
+});
+window.addEventListener("contextmenu", (e) => {
+  e.preventDefault();
+});
+window.addEventListener("mousedown", (e) => {
+  if (e.button == 2) {
+    isPanning = true;
+    panning.startX = mouse.x;
+    panning.startY = mouse.y;
+    document.body.style.cursor = "grabbing";
+  }
+});
+window.addEventListener("mouseup", (e) => {
+  if (e.button == 2) {
+    isPanning = false;
+    document.body.style.cursor = "default";
+  }
+});
+window.addEventListener("mousemove", (e) => {
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+});
+window.addEventListener("wheel", (e) => {
+  scale.x = clamp(scale.x - e.deltaY / 1000, 0.1, 5);
+  scale.y = clamp(scale.y - e.deltaY / 1000, 0.1, 5);
+  console.log(scale);
+});
 
-// animate();
+animate();
+
+function clamp(value, min, max) {
+  return Math.min(Math.max(value, min), max);
+}
