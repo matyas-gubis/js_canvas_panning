f3906b66bb31c73146b964fda10929a594ebad98
matyasgubis <gubismatyas@gmail.com>
2024-03-28 20:06:30 +0100

    feat: init socket io

--- src/public/main.js
+++ src/public/main.js
@@ -1,5 +1,6 @@
 import { Game } from './game.js';
 
+const socket = io();
 const canvas = document.getElementById('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -15,14 +16,3 @@ const hudCtx = hudCanvas.getContext('2d');
 const game = new Game(canvas.width, canvas.height, ctx, hudCtx);
 
 game.init(canvas);
-
-let lastTime = 0;
-function animate(timeStamp) {
-    const deltaTime = timeStamp - lastTime;
-    lastTime = timeStamp;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // console.log(deltaTime);
-    game.render(ctx, deltaTime, hudCtx);
-    requestAnimationFrame(animate);
-}
-// animate(lastTime);
